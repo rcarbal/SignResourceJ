3d423e0db8bba513a7279040731f97871c3c383c
Ricardo Carballo <rcarbaleq2@gmail.com>
2018-11-18 23:26:07 -0800

    Discrepancies UI, Test extraction comple pass

--- src/module-info.java
+++ src/module-info.java
@@ -3,6 +3,8 @@ module SignResourceJ {
     requires javafx.fxml;
     requires javafx.controls;
     requires java.datatransfer;
+    requires poi.ooxml;
+    requires poi;
 
     opens sample;
 }
---
+++ test/PurchaseOrderUtils.java
@@ -0,0 +1,47 @@
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.junit.jupiter.api.Test;
+import pojo.DescrepencyParse;
+import pojo.PurchaseOrder;
+import utils.ProcessPODescrepencies;
+import utils.PurchaseOrderExtractor;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PurchaseOrderUtils {
+
+    @Test
+    void extractPurchaseOrdersToArray(){
+
+        ArrayList<PurchaseOrder> purchaseOrder = null;
+        try {
+            purchaseOrder = PurchaseOrderExtractor.
+                    getPurchaseOrder("C:/Users/rcarb/OneDrive/Documents/testExcel.xlsx");
+        } catch (IOException | ParseException | InvalidFormatException e) {
+            e.printStackTrace();
+        }
+        assert purchaseOrder != null;
+        assertTrue(purchaseOrder.size()>0);
+
+    }
+
+    @Test
+    void comparePreceDiscrepencies(){
+
+        //standard call for program.
+        ArrayList<PurchaseOrder> purchaseOrder = null;
+        try {
+            purchaseOrder = PurchaseOrderExtractor.
+                    getPurchaseOrder("C:/Users/rcarb/OneDrive/Documents/testExcel.xlsx");
+        } catch (IOException | ParseException | InvalidFormatException e) {
+            e.printStackTrace();
+        }
+
+        DescrepencyParse descrepencies = ProcessPODescrepencies.getDescrepencies(purchaseOrder);
+        assertTrue(descrepencies.getArray().size() >0);
+    }
+
+}
